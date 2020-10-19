1ea82a39416963d997be0da363fba7a3f18da7a9
tanvimane228 <tanvimane228@gmail.com>
2020-10-19 14:57:13 +0530

    Updates

---
+++ precache-manifest.2679fc7351b6b9c8b651eebc51c7cf67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ea21e64f0d68253e0e4b06b20d6fef",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "cde57923972a3a798ae5",
+    "url": "/react-app1/static/css/main.86d9a2fd.chunk.css"
+  },
+  {
+    "revision": "af10d5956c7d7fb1c9dc",
+    "url": "/react-app1/static/js/2.1d4e64dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app1/static/js/2.1d4e64dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde57923972a3a798ae5",
+    "url": "/react-app1/static/js/main.e682744e.chunk.js"
+  },
+  {
+    "revision": "b2090f245e3ec4d6b39c",
+    "url": "/react-app1/static/js/runtime-main.355eaa59.js"
+  }
+]);
